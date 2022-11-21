c8e6db6cbcf6cf26eb04f6f5f5c26c2b9faee8a3
Kevin Faveri <kevin@faveri.dev>
2022-11-21 19:24:47 -0300

    disable refreshing hash list with cronjob

--- cronjobs/revealMintsCronjob.ts
+++ cronjobs/revealMintsCronjob.ts
@@ -1,16 +1,17 @@
 import cron from 'node-cron';
-import { refreshHashLists } from '../scripts/refreshHashLists';
+// import { refreshHashLists } from '../scripts/refreshHashLists';
 import { revealAllMintedNFTS } from '../scripts/revealAllMintedNFTS';
 
-cron.schedule('30 * * * * *', async () => {
-  console.info(
-    `Cron "refreshHashLists" running again ${new Date().toLocaleString()}`,
-  );
-  await refreshHashLists().catch((e) => console.error(e));
-  console.info(
-    `Cron "refreshHashLists" finished ${new Date().toLocaleString()}`,
-  );
-});
+// TODO: Remove in the near future as we now rely on Helius webbhook
+// cron.schedule('30 * * * * *', async () => {
+//   console.info(
+//     `Cron "refreshHashLists" running again ${new Date().toLocaleString()}`,
+//   );
+//   await refreshHashLists().catch((e) => console.error(e));
+//   console.info(
+//     `Cron "refreshHashLists" finished ${new Date().toLocaleString()}`,
+//   );
+// });
 
 cron.schedule('59 * * * * *', async () => {
   console.info(
