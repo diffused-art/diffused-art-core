234bc4e4636e4e5994ec8f5bbe6d484c8ede2b58
Kevin Faveri <kevin@faveri.dev>
2022-11-24 12:00:24 -0300

    fix build dreamstudio

--- scripts/download-and-build-sd-lib.sh
+++ scripts/download-and-build-sd-lib.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # sudo rm -rf ./api-interfaces
 # git clone git@github.com:Stability-AI/api-interfaces.git
+sudo chmod -R 777 ./binaries/protoc
+sudo chmod -R 777 ./api-interfaces/src/bin/protoc
 cp ./binaries/protoc ./api-interfaces/src/bin/protoc
 cd ./api-interfaces
 cmake .
@@ -12,4 +14,7 @@ cp ./gooseai/generation/generation_pb_service.d.ts ../functions/ai-sources/stabl
 cp ./gooseai/generation/generation_pb_service.js ../functions/ai-sources/stable-diffusion/stubs/generation_pb_service.js
 cp ./gooseai/generation/generation_pb.d.ts ../functions/ai-sources/stable-diffusion/stubs/generation_pb.d.ts
 cp ./gooseai/generation/generation_pb.js ../functions/ai-sources/stable-diffusion/stubs/generation_pb.js
+cp ./gooseai/generation/tensors_pb.js ../functions/ai-sources/stable-diffusion/stubs/tensors_pb.js
+cp ./gooseai/generation/tensors_pb.d.ts ../functions/ai-sources/stable-diffusion/stubs/tensors_pb.d.ts
+
 echo "Copied files"
