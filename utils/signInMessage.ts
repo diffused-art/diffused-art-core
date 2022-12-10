c6b033e05a8211d19760bcd207676473b9c11f75
Kevin Faveri <kevin@faveri.dev>
2022-12-10 18:17:08 -0300

    feat: tweaks signInMessage

--- utils/signInMessage.ts
+++ utils/signInMessage.ts
@@ -5,7 +5,7 @@ export default function signInMessage(params: {
   return `diffused.art wants you to sign in with your Solana account:
   ${params.walletAddress}
 
-  Click Sign or Approve only means you have proved this wallet is owned by you. This request will not trigger any blockchain transaction or cost any gas fee. Use of our website and service are subject to our Terms of Service: https://diffused.art/terms-of-service.pdf and Privacy Policy: https://diffused.art/privacy-policy.pdf
+  Click Sign only means you have proved this wallet is owned by you. This request will not trigger any blockchain transaction. Website and service use are subject to our Terms of Service: https://diffused.art/terms-of-service.pdf and Privacy Policy: https://diffused.art/privacy-policy.pdf
 
   URI: https://diffused.art
   Version: 1
